3b55ee75bb6c4b31409c7d541aac35d28d7ff593
Breno <59184811+Breno30@users.noreply.github.com>
2020-12-31 08:52:28 -0300

    Update conf-db.js

--- conf-db.js
+++ conf-db.js
@@ -1,19 +1,1 @@
 
-    <script>
-
-        const firebaseConfig = {
-          apiKey: "AIzaSyCITTLNZ77huYnW8eEawnogYuSkYGf8S6E",
-          authDomain: "testing-6f6b3.firebaseapp.com",
-          databaseURL: "https://testing-6f6b3.firebaseio.com",
-          projectId: "testing-6f6b3",
-          storageBucket: "testing-6f6b3.appspot.com",
-          messagingSenderId: "273326537847",
-          appId: "1:273326537847:web:7017d27e4ed81dc3f09a72",
-          measurementId: "G-C37CCMR66T"
-        };
-
-        firebase.initializeApp(firebaseConfig);
-        firebase.analytics();
-
-
-    </script>
